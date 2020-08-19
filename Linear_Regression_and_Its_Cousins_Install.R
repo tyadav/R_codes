5e18b2c0cae563cd79c7854f6eeeb130291f52e7
Tej <tej.narayan@gmail.com>
2020-08-19 20:52:34 +0530

    Package install

---
+++ Linear_Regression_and_Its_Cousins_Install.R
@@ -0,0 +1,9 @@
+
+## You may be prompted to choose a CRAN destination from a list of
+## availible websites to download the files
+
+install.packages(c("caret", "AppliedPredictiveModeling", "corrplot",
+                   "pls", "elasticnet"),
+                 dependencies = c("Depends", "Suggests"))
+
+
